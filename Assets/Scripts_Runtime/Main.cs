aff099e2a66a41f0e18411cb1ea8d21687093ccc
Jackutea <chenwansal1@163.com>
2024-05-21 20:13:19 +0800

    ESTABLISH

---
+++ Assets/Scripts_Runtime/Main.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelEditorTutorial {
+
+    public class Main : MonoBehaviour {
+
+        void Awake() {
+            Debug.Log("Main.Awake()");
+        }
+
+        void Update() {
+
+        }
+    
+    }
+
+}
